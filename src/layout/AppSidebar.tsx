9acf6e1081a2ff274e4c898e85b8962bdae05c11
Bhavan <bhavankumar.n@webtiga.com>
2026-07-17 15:58:44 +0530

    Logo Dark added

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -319,7 +319,7 @@ const AppSidebar: React.FC = () => {
               />
               <Image
                 className="hidden dark:block"
-                src="/images/logo/logo-dark.svg"
+                src="/images/syn_logo.png"
                 alt="Logo"
                 width={150}
                 height={40}
